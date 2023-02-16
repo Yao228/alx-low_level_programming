839ef3456b5b88fae81bb9e45e201ebb08b0676c
Yao228 <yamedekpedzi@gmail.com>
2023-02-15 18:54:58 -0800

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t size = 0;
+	size_t size;
 
+	size = 0;
 	while (h)
 	{
-		size++;
 		printf("%d\n", h->n);
 		h = h->next;
-		
+		++size;
 	}
 
 	return (size);
