1ad92503819f2afeed98628a87dce5a504097c31
root <yamedekpedzi@gmail.com>
2023-05-24 08:25:28 -0700

    Updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/yao228/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P ../ https://github.com/Yao228/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=../libhack.so
